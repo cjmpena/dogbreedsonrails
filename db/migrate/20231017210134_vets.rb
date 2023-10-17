fc4a65dd90ee85c87963d85eb2c5809b1f0c5348
John Paul Villanueva <jpvillanueva2000@gmail.com>
2023-10-17 18:18:43 -0500

    vets

--- db/migrate/20231017210134_vets.rb
+++ db/migrate/20231017210134_vets.rb
@@ -1,4 +1,9 @@
-class Vets < ActiveRecord::Migration[7.0]
+class CreateVets < ActiveRecord::Migration[7.0]
   def change
+    create_table :vets do |t|
+      t.integer :id, primary_key: true
+      t.string :name
+      t.timestamps
+    end
   end
 end
