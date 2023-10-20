a70cab57b109b08110a7372c3b40e518480fbb9b
Carla <manansal.carla@gmail.com>
2023-10-20 12:40:50 -0500

    comments

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,8 @@
 class Owner < ApplicationRecord
     has_many :owner_dog_breeds
     has_many :dog_breeds, through: :owner_dog_breeds
+    
+    # Validations
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,24 @@
 # require 'httparty'
 # require 'json'
 
-# # Load the Rails environment
-# require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 
-# # Define the DogBreed model class
-# class DogBreed < ApplicationRecord
-# end
+class DogBreed < ApplicationRecord
+end
 
-# response = HTTParty.get('https://dog.ceo/api/breeds/list/all')
+response = HTTParty.get('https://dog.ceo/api/breeds/list/all')
 
-# if response.code == 200
-#   data = JSON.parse(response.body)
+if response.code == 200
+  data = JSON.parse(response.body)
 
-#   # Assuming the data structure from the API is {"message": {"breed_name": [...sub_breeds]}}
-#   data['message'].each do |breed, _sub_breeds|
-#     DogBreed.create(name: breed)
-#   end
+  data['message'].each do |breed, _sub_breeds|
+    DogBreed.create(name: breed)
+  end
 
-#   puts 'Data imported successfully'
-# else
-#   puts 'Failed to fetch data from the API'
-# end
+  puts 'Data imported successfully'
+else
+  puts 'Failed to fetch data from the API'
+end
 
 # owners_data = [
 #     { name: 'Johnathan Smith', email: 'johnathan.smith@mailcompany.com', phone: '+44 20 1234 5678' },
