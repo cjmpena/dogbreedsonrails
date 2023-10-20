eccb15bc43a0e895ef9915f5b619c097ff323ca3
John Paul Villanueva <jpvillanueva2000@gmail.com>
2023-10-20 12:39:51 -0500

    updated designs for all pages and added pagination

--- app/controllers/dog_breeds_controller.rb
+++ app/controllers/dog_breeds_controller.rb
@@ -1,11 +1,11 @@
 class DogBreedsController < ApplicationController
-    def show
-      @query = params[:query]
-      
-      @dog_breeds = if @query.present?
-                      DogBreed.where('name LIKE ?', "%#{@query}%")
-                    else
-                      DogBreed.all
-                    end
-    end
+  def show
+    @query = params[:query]
+
+    @dog_breeds = if @query.present?
+                    DogBreed.where('name LIKE ?', "%#{@query}%").paginate(page: params[:page], per_page: 25)
+                  else
+                    DogBreed.paginate(page: params[:page], per_page: 25)
+                  end
   end
+end
