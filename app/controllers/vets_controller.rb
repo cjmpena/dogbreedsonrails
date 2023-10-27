05b6c303dc718d843cf1bef69f856f80496438f0
John Paul Villanueva <jpvillanueva2000@gmail.com>
2023-10-26 21:09:20 -0500

    added hierarchical search to the vets page

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -1,6 +1,18 @@
 class VetsController < ApplicationController
+  before_action :specialties, only: [:show]
+
   def show
-    @vets = Vet.paginate(page: params[:page], per_page: 5)
+    @vets = Vet.all
+
+    if params[:query].present?
+      @vets = @vets.where("name LIKE ?", "%#{params[:query]}%")
+    end
+
+    if params[:specialty].present?
+      @vets = @vets.where(specialty: params[:specialty])
+    end
+
+    @vets = @vets.paginate(page: params[:page], per_page: 5)
   end
 
   def specialty
@@ -10,5 +22,11 @@ class VetsController < ApplicationController
   def by_specialty
     @specialty = params[:specialty]
     @vets = Vet.where(specialty: @specialty)
+  end
+
+  private
+
+  def specialties
+    @specialties = Vet.select(:specialty).distinct.pluck(:specialty)
   end
 end
