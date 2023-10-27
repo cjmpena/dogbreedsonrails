fab8b53de24d322a3dddfec3a125ebc6e79e45ec
John Paul Villanueva <jpvillanueva2000@gmail.com>
2023-10-26 21:59:28 -0500

    added hierarchical navigation and search functionality to the vets page

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -1,8 +1,30 @@
 class VetsController < ApplicationController
+  before_action :specialties, only: [:show]
+
   def show
     @vets = Vet.paginate(page: params[:page], per_page: 20)
+
+    if params[:query].present?
+      @vets = @vets.where("name LIKE ?", "%#{params[:query]}%")
+    end
+
+    if params[:specialty].present?
+      @vets = @vets.where(specialty: params[:specialty])
+    end
   end
+
   def specialty
     @vet = Vet.find(params[:id])
+  end
+
+  def by_specialty
+    @specialty = params[:specialty]
+    @vets = Vet.where(specialty: @specialty)
+  end
+
+  private
+
+  def specialties
+    @specialties = Vet.select(:specialty).distinct.pluck(:specialty)
   end
 end
