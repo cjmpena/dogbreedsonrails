9b49a3d88758948b465ca6be63f02b11e0643a8d
John Paul Villanueva <jpvillanueva2000@gmail.com>
2023-10-26 19:20:27 -0500

    added hierarchical navigation to the vets page

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -1,9 +1,14 @@
 class VetsController < ApplicationController
-    def show
-      @vets = Vet.paginate(page: params[:page], per_page: 5)
-    end
-    def specialty
-      @vet = Vet.find(params[:id])
-    end
+  def show
+    @vets = Vet.paginate(page: params[:page], per_page: 5)
   end
-  
+
+  def specialty
+    @vet = Vet.find(params[:id])
+  end
+
+  def by_specialty
+    @specialty = params[:specialty]
+    @vets = Vet.where(specialty: @specialty)
+  end
+end
